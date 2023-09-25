c401bb5fafb90c16c649695f3fbcd515dd5330ee
Bautista Dev <76666780+Bautistadev@users.noreply.github.com>
2023-09-25 09:20:14 -0300

    TEST: Testeo de la clase UserService

--- src/test/java/com/example/springSecurity/part1/Service/UserServiceTest.java
+++ src/test/java/com/example/springSecurity/part1/Service/UserServiceTest.java
@@ -1,4 +1,46 @@
+package com.example.springSecurity.part1.Service;
+
+import com.example.springSecurity.part1.Entity.User;
+import com.example.springSecurity.part1.Repository.UserRepositoryImplements;
+import com.example.springSecurity.part1.dto.UserDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
-  
+
+    @Mock
+    private UserRepositoryImplements userRepositoryImplements
+    ;@InjectMocks
+    private UserService service;
+
+    private UserDTO userDTO;
+
+    @BeforeEach
+    void setUp() {
+        userDTO = new UserDTO();
+        userDTO.id(new BigDecimal(1L));
+        userDTO.firstName("admin");
+        userDTO.userName("admin");
+        userDTO.lastName("admin");
+        userDTO.password("admin");
+        userDTO.dateCreated("2023-09-24 20:16:43.798590");
+        userDTO.dateDeleted(null);
+
+    }
+
+    @Test
+    void findById() {
+
+    }
 }
